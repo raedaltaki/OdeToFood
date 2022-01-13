f31019d23146ae39a982309f50dfb4b5df57e9ed
Raed Altaki <raed.simon@gmail.com>
2022-01-13 01:09:33 -0500

    initiate project

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1 , Name="Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2 , Name="Raed's Pizza", Location="Mississuaga", Cuisine=CuisineType.Mexican},
+                new Restaurant { Id = 3 , Name="Emad's Pizza", Location="Toronto", Cuisine=CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
