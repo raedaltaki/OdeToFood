843dc4f5050e50e7de2e72c2278eb34ed3496816
raedaltaki <raed.simon@gmail.com>
2022-02-12 02:03:28 -0500

    add delete feature and count view

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -14,6 +14,7 @@ namespace OdeToFood.Data
         Restaurant Update(Restaurant updateRestaurant);
         Restaurant Add(Restaurant newRestaurant);
         Restaurant Delete(int id);
+        int GetCountOfRestaurants();
         int Commit();
     }
 }
---
+++ OdeToFood/Pages/ViewComponents/RestaurantCountViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.ViewComponents
+{
+    public class RestaurantCountViewComponent
+        : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestaurantCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCountOfRestaurants();
+            return View(count);
+        }
+
+    }
+}
